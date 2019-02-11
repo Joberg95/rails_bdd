52fd95baf6dfb6ce9c8b68f2d4fb06e380181563
nevroje <joakim.berglind@gmail.com>
2019-02-11 17:53:18 +0100

    added a breaking new article and got 3 passing tests

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,6 +2,6 @@ When("I visit the site") do
     visit root_path
 end
 
-Then("I should see {string}") do |string|
+Then("I should see {string}") do |content|
     expect(page).to have_content content
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,14 +13,9 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
-
 end
 
 Shoulda::Matchers.configure do |config|
