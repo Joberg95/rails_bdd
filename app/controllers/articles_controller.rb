481a7a85aa9cda3773226648f8abd523941779b5
nevroje <joakim.berglind@gmail.com>
2019-02-23 10:02:31 +0100

    created articles in rails console

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.create(article_params)
     if @article.persisted?
-      redirect_to root_path, notice: 'Your article has been created'
+      redirect_to root_path, notice: 'Article was successfully created.'
     else
-      flash[:error] = "Your Article could not be saved"
+      flash[:error] = ""
       render :new
     end
   end
